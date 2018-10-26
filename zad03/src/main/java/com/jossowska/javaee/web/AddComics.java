ff4a3fef1150019eaba35d404373ea4f49cc0655
Jagoda Ossowska <jossowska@sigma.ug.edu.pl>
2018-10-26 12:34:39 +0200

    update

--- zad03/src/main/java/com/jossowska/javaee/web/AddComics.java
+++ zad03/src/main/java/com/jossowska/javaee/web/AddComics.java
@@ -19,7 +19,18 @@ public class AddComics extends HttpServlet{
 		response.setContentType("text/html");
 		
 		PrintWriter out = response.getWriter();
-		out.println("<html><body><h2></h2></body></html>");
+		out.println("<html><body><h2>Add new comics here!</h2>" +
+				"				\"<form action='all-comics +\n" + 
+				"				\"Title: <input type='text' name='title'' /> <br />\" +\n" + 
+				"				\"Price: <input type='text' name='price' /> <br />\" +\n" + 
+				"				\"Date of release: <input type='text' name='dateOfRelease' /> <br />\" +\n" + 
+				"				\"Is this a popular comics?: <select name='isPopular'>\" +\n" + 
+				"				\"<option value='true'>Yes</option>\" +\n" + 
+				"				\"<option value='false'>No</option>\" +\n" +
+				"				\"</select><br />\" +\n" + 
+				"				\"<input type='submit' value=' Add comics' />\" +\n" + 
+				"				\"</form>" + 
+				"</body></html>");
 		
 		out.close();
 	}
